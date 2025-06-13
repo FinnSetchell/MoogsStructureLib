cac570169bcaf8f85a850c0be279c9fd757e319e
finnsetchell <finn@setchell.org>
2025-06-13 16:27:43 +0100

    ported to 1.21.4

--- neoforge/src/main/java/com/finndog/moogs_structures/datagen/StructureNbtUpdaterDatagen.java
+++ neoforge/src/main/java/com/finndog/moogs_structures/datagen/StructureNbtUpdaterDatagen.java
@@ -2,9 +2,10 @@ package com.finndog.moogs_structures.datagen;
 
 import com.finndog.moogs_structures.MoogsStructuresCommon;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 // Source: https://github.com/BluSunrize/ImmersiveEngineering/blob/1.20.1/src/datagen/java/blusunrize/immersiveengineering/data/IEDataGenerator.java
@@ -12,13 +13,10 @@ import net.neoforged.neoforge.data.event.GatherDataEvent;
 public class StructureNbtUpdaterDatagen {
 
     @SubscribeEvent
-    public static void gatherData(GatherDataEvent event) {
-        ExistingFileHelper exHelper = event.getExistingFileHelper();
+    public static void gatherData(GatherDataEvent.Server event) {
+        ResourceManager resourceManager = event.getResourceManager(PackType.SERVER_DATA);
         DataGenerator gen = event.getGenerator();
         final var output = gen.getPackOutput();
-
-        if (event.includeServer()) {
-            gen.addProvider(true, new StructureNbtUpdater("structures", MoogsStructuresCommon.MODID, exHelper, output));
-        }
+        gen.addProvider(true, new StructureNbtUpdater("structure", MoogsStructuresCommon.MODID, resourceManager, output));
     }
 }
