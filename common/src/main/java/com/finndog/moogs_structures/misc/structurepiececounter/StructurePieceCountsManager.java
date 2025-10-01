8d72abccbbcc4ab4ace01f8385f7d8f3b92a299c
joshieman <71846165+joshieman06@users.noreply.github.com>
2025-10-01 11:29:27 -0700

    Update StructurePieceCountsManager.java

--- common/src/main/java/com/finndog/moogs_structures/misc/structurepiececounter/StructurePieceCountsManager.java
+++ common/src/main/java/com/finndog/moogs_structures/misc/structurepiececounter/StructurePieceCountsManager.java
@@ -6,9 +6,11 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.resources.FileToIdConverter;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
+import net.minecraft.util.ExtraCodecs;
 import net.minecraft.util.profiling.ProfilerFiller;
 import org.jetbrains.annotations.Nullable;
 
