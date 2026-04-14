3011c8208525291e9b8b277dacd946917687bfc5
finnsetchell <finn@setchell.org>
2026-04-14 11:43:47 +0200

    added forge module

---
+++ forge/src/main/java/com/finndog/moogs_structures/modinit/registry/forge/ResourcefulRegistriesImpl.java
@@ -0,0 +1,72 @@
+package com.finndog.moogs_structures.modinit.registry.forge;
+
+import com.finndog.moogs_structures.modinit.registry.CustomRegistryLookup;
+import com.finndog.moogs_structures.modinit.registry.ResourcefulRegistry;
+import com.finndog.moogs_structures.platform.IRegistryPlatform;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraftforge.event.registries.NewRegistryEvent;
+import net.minecraftforge.registries.RegistryBuilder;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class ResourcefulRegistriesImpl implements IRegistryPlatform {
+
+    private static final List<CustomRegistryInfo<?, ?>> CUSTOM_REGISTRIES = new ArrayList<>();
+
+    @Override
+    public <T> ResourcefulRegistry<T> create(Registry<T> registry, String id) {
+        return new ForgeResourcefulRegistry<>(registry, id);
+    }
+
+    @Override
+    public <T, K extends Registry<T>> Pair<Supplier<CustomRegistryLookup<T>>, ResourcefulRegistry<T>> createCustomRegistryInternal(String modId, ResourceKey<K> key, boolean save, boolean sync, boolean allowModification) {
+        CustomRegistryInfo<T, T> info = new CustomRegistryInfo<>(new LateSupplier<>(), key, save, sync, allowModification);
+        CUSTOM_REGISTRIES.add(info);
+        return Pair.of(info.lookup(), new ForgeResourcefulRegistry<>(key, modId));
+    }
+
+    public static void onRegisterForgeRegistries(NewRegistryEvent event) {
+        CUSTOM_REGISTRIES.forEach(registry -> registry.build(event));
+    }
+
+    public static class LateSupplier<T> implements Supplier<T> {
+        private T value;
+        private boolean initialized = false;
+
+        public void set(T value) {
+            this.value = value;
+            this.initialized = true;
+        }
+
+        @Override
+        public T get() {
+            if (!initialized) {
+                throw new IllegalStateException("LateSupplier not initialized");
+            }
+            return value;
+        }
+    }
+
+    public record CustomRegistryInfo<T, K extends T>(
+            LateSupplier<CustomRegistryLookup<T>> lookup,
+            ResourceKey<? extends Registry<T>> key,
+            boolean save,
+            boolean sync,
+            boolean allowModification
+    ) {
+
+        public void build(NewRegistryEvent event) {
+            lookup.set(new ForgeCustomRegistry<>(event.create(getBuilder())));
+        }
+
+        public RegistryBuilder<T> getBuilder() {
+            return new RegistryBuilder<>(key)
+                    .disableRegistrationCheck()
+                    .sync(sync);
+        }
+    }
+}
