7f9d7b52cf0abbfede0fa0e163b9e6cbf6d1e3d7
finnsetchell <finn@setchell.org>
2026-05-27 16:46:43 +0200

    Add basalt and delta suppression within structure piece bounds

---
+++ common/src/main/java/com/finndog/moogs_structures/mixins/features/NoBasaltColumnsInStructuresMixin.java
@@ -0,0 +1,28 @@
+package com.finndog.moogs_structures.mixins.features;
+
+import com.finndog.moogs_structures.modinit.MoogsStructuresTags;
+import com.finndog.moogs_structures.utils.MixinUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.WorldGenRegion;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.levelgen.feature.BasaltColumnsFeature;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BasaltColumnsFeature.class)
+public class NoBasaltColumnsInStructuresMixin {
+    @Inject(
+            method = "canPlaceAt(Lnet/minecraft/world/level/LevelAccessor;ILnet/minecraft/core/BlockPos$MutableBlockPos;)Z",
+            at = @At(value = "HEAD"),
+            cancellable = true
+    )
+    private static void moogs_structures_noBasaltColumnsInStructures(LevelAccessor levelAccessor, int seaLevel, BlockPos.MutableBlockPos mutableBlockPos, CallbackInfoReturnable<Boolean> cir) {
+        if (!(levelAccessor instanceof WorldGenRegion worldGenRegion)) return;
+
+        if (MixinUtils.isPositionInTaggedStructure(worldGenRegion, mutableBlockPos, MoogsStructuresTags.NO_BASALT)) {
+            cir.setReturnValue(false);
+        }
+    }
+}
