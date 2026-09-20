67b2c2424922a6c9d1d9228be5afe7c7323f9f41
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2026-09-20 19:07:24 +0200

    point discord link at tracked redirect

--- common/src/main/java/com/finndog/moogs_structures/client/SupportLinks.java
+++ common/src/main/java/com/finndog/moogs_structures/client/SupportLinks.java
@@ -21,7 +21,7 @@ public final class SupportLinks {
 
     private static final String DISCORD_ID = "discord";
     private static final String KOFI_ID = "kofi";
-    private static final String DISCORD_URL = "https://discord.gg/S5nffJbuvA";
+    private static final String DISCORD_URL = "https://moogsmods.com/discord?r=ingame-msl";
     private static final String KOFI_URL = "https://ko-fi.com/finndog";
     private static final ResourceLocation DISCORD_TEX = new ResourceLocation(MoogsStructuresCommon.MODID, "textures/gui/discord.png");
     private static final ResourceLocation KOFI_TEX = new ResourceLocation(MoogsStructuresCommon.MODID, "textures/gui/kofi.png");
